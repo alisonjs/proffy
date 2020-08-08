bfe6d90304396a4420a82436d897cd4d8c940038
Alison Sousa <alison.dev.silva@gmail.com>
2020-08-07 23:45:26 -0300

    Screenshots

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -22,33 +22,48 @@ function Landing(){
 
     return(
         <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <div className="logo-container">
-                    <img src={logoImg} alt="proffy"/>
-                    <h2>Sua plataforma de estudos online.</h2>
-                </div>
+            <div id="page-landing-content">
+                <main>
+                    <div className="logo-container">
+                        <img src={logoImg} alt="proffy"/>
+                        <h2>Sua plataforma de estudos online.</h2>
+                    </div>
+                    <div className="image-container">
+                        <img 
+                            src={landingImg} 
+                            alt="Plataforma de estudos" 
+                            className="hero-image"
+                        />
+                    </div>
+                </main>
+                
+                <footer>
 
-                <img 
-                    src={landingImg} 
-                    alt="Plataforma de estudos" 
-                    className="hero-image"
-                />
+                    <div className="info">
+                        <div className="welcome-message">
+                            <span>
+                                Seja bem vindo. <br />
+                                <strong>O que deseja fazer</strong>
+                            </span>
+                        </div>
 
-                <div className="buttons-container">
-                    <Link to="/study" className="study">
-                        <img src={studyIcon} alt="Estudar"/>
-                        Estudar
-                    </Link>
+                        <span className="total-connections">
+                            Total de { totalConnections } conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/>
+                        </span>
+                    </div>
 
-                    <Link to="/give-classes" className="give-classes">
-                        <img src={giveClassesIcon} alt="Dar aulas"/>
-                        Dar aulas
-                    </Link>
-                </div>
+                    <div className="buttons-container">
+                        <Link to="/study" className="study">
+                            <img src={studyIcon} alt="Estudar"/>
+                            Estudar
+                        </Link>
 
-                <span className="total-connections">
-                    Total de { totalConnections } conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/>
-                </span>
+                        <Link to="/give-classes" className="give-classes">
+                            <img src={giveClassesIcon} alt="Dar aulas"/>
+                            Dar aulas
+                        </Link>
+                    </div>
+                </footer>
             </div>
         </div>
     );
